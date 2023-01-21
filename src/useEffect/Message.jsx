50b8ee05bdbc078f8ecdd55bcee20a5333161ddd
Leonardo Puebla <leonardo38361@gmail.com>
2023-01-20 22:34:23 -0300

    useEffect cleanup o efecto con saneamiento

--- src/useEffect/Message.jsx
+++ src/useEffect/Message.jsx
@@ -1,17 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Message = () => {
-  useEffecteEffect(() => {
-    console.log('Message Mounted');
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+    const onMouseMove = ({ x, y }) => {
+      //   const coords = { x, y };
+      setCoords({ x, y });
+    };
+
+    window.addEventListener('mousemove', onMouseMove);
 
     return () => {
-      console.log('Message Unmounted');
+      window.removeEventListener('mousemove', onMouseMove);
     };
   }, []);
 
   return (
     <>
       <h3>Usuario ya existe</h3>
+      {JSON.stringify(coords)}
     </>
   );
 };
