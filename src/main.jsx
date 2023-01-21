7c8a11370b1d6cc30c32617575ff1ad4c47e47e8
Leonardo Puebla <leonardo38361@gmail.com>
2023-01-20 22:58:23 -0300

    custom hook para formulario, useForm

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import './index.css'
 // import HooksApp from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
-import { SimpleForm } from './useEffect/SimpleForm'
+// import { SimpleForm } from './useEffect/SimpleForm'
+import { FormWithCustomHook } from './useEffect/FormWithCustomHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SimpleForm />
+    <FormWithCustomHook />
   </React.StrictMode>,
 )
