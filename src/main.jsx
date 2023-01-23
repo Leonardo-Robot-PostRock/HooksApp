0fb6666025542cbd6b38495df259a51beb1dceb3
Leonardo Puebla <leonardo38361@gmail.com>
2023-01-23 20:56:32 -0300

    Multiple custom hooks

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,11 @@ import './index.css'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './useEffect/SimpleForm'
-import { FormWithCustomHook } from './useEffect/FormWithCustomHook'
+// import { FormWithCustomHook } from './useEffect/FormWithCustomHook'
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <FormWithCustomHook />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <MultipleCustomHooks />
+  // </React.StrictMode>,
 )
