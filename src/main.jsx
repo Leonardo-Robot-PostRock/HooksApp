041a90b35532d34f97adc3f05316fb1d591b53ea
Leonardo Puebla <leonardo38361@gmail.com>
2023-01-24 14:20:14 -0300

    useCallback

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-// import HooksApp from './HooksApp'
-// import { CounterApp } from './01-useState/CounterApp'
-// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
-// import { SimpleForm } from './useEffect/SimpleForm'
-// import { FormWithCustomHook } from './useEffect/FormWithCustomHook'
-// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
-// import { FocusScreen } from './04-useRef/FocusScreen'
-// import { Layout } from './05-useLayoutEffect/Layout'
-// import { Memorize } from './06-memos/Memorize'
-import { MemoHook } from './06-memos/MemoHook'
+// import HooksApp from './HooksApp';
+// import { CounterApp } from './01-useState/CounterApp';
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+// import { SimpleForm } from './useEffect/SimpleForm';
+// import { FormWithCustomHook } from './useEffect/FormWithCustomHook';
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+// import { FocusScreen } from './04-useRef/FocusScreen';
+// import { Layout } from './05-useLayoutEffect/Layout';
+// import { Memorize } from './06-memos/Memorize';
+// import { MemoHook } from './06-memos/MemoHook';
+import { CallbackHook } from './06-memos/CallbackHook';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <MemoHook />
+    <CallbackHook />
   // </React.StrictMode>,
 )
