ac735dbcee2a75466970823f7eb1387e78770d19
Leonardo Puebla <leonardo38361@gmail.com>
2023-01-24 23:07:27 -0300

    map de todos

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -15,7 +15,7 @@ export const TodoApp = () => {
     }
   ];
   const [todos, dispatchTodo] = useReducer(todoReducer, initialState);
-
+  console.log(todos);
   return (
     <>
       <h1>
@@ -27,10 +27,12 @@ export const TodoApp = () => {
       <div className="row">
         <div className="col-7">
           <ul className="list-group">
-            <li className="list-group-item d-flex justify-content-between">
-              <span className="align-self-center">Item 1</span>
-              <button className="btn btn-danger">Borrar</button>
-            </li>
+            {todos.map((todo) => (
+              <li key={todo.id} className="list-group-item d-flex justify-content-between">
+                <span className="align-self-center">Item 1</span>
+                <button className="btn btn-danger">Borrar</button>
+              </li>
+            ))}
           </ul>
         </div>
 
