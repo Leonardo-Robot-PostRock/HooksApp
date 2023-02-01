7898c6d8811d9e3ac50b8adb4d71f0fe583c28f0
Leonardo Puebla <leonardo38361@gmail.com>
2023-02-01 02:35:59 -0300

    useContext

--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,11 +1,8 @@
+import { useState } from 'react';
 import { UserContext } from './UserContext';
 
-const user = {
-    id: 123,
-    name: 'Leonardo Puebla',
-    email: 'leonardo@example.com'
-}
-
 export const UserProvider = ({ children }) => {
-  return <UserContext.Provider value={{ hola: 'Mundo', user: user }}>{children}</UserContext.Provider>;
+  const [user, setUser] = useState();
+
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
 };
