0f1d8d0c4adc7687199d8b16a73f64df12a0ac42
Leonardo Puebla <leonardo38361@gmail.com>
2023-02-02 23:06:15 -0300

    espacios

--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -5,6 +5,7 @@ export const todoReducer = (initialState = [], action) => {
 
         case 'Remove Todo':
             return initialState.filter(todo => todo.id !== action.payload);
+
         case 'Toggle Todo':
             return initialState.map(todo => {
                 if (todo.id === action.payload) {
@@ -15,6 +16,7 @@ export const todoReducer = (initialState = [], action) => {
                 }
                 return todo
             })
+
         default:
             return initialState
     }
